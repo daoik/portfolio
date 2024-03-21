595eeb6f5d80d0eba531d188fda2ba1e7d49cc6e
daoik <dimeconomu@gmail.com>
2024-03-21 20:53:03 +0200

    add 1 blob

--- src/components/Blob.tsx
+++ src/components/Blob.tsx
@@ -54,6 +54,17 @@ const Blob = () => {
           className=" fill-pink-400 opacity-80  noise"
         ></path>
       </svg>
+      <svg
+        className="absolute  -z-10 top-0 right-0 "
+        viewBox="0 0 400 900"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d={paths[5]}
+          transform="translate(100 620)"
+          className=" fill-pink-400 opacity-80  noise"
+        ></path>
+      </svg>
     </div>
   );
 };
